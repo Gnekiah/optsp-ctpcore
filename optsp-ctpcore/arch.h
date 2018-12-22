2ddd3575c14d126734f7249e3337e9b5708e82b6
Xxiong <xxiong@cqu.edu.cn>
2018-12-22 17:26:12 +0800

    update arch

--- optsp-ctpcore/arch.h
+++ optsp-ctpcore/arch.h
@@ -1,18 +1,43 @@
 #ifndef OPTSP_CTPCORE_ARCH_H_
 #define OPTSP_CTPCORE_ARCH_H_
 
-#include <boost/date_time/posix_time/posix_time.hpp>
+#include <stdint.h>
+#include <cstring>
+
+#if defined(__linux__) && defined( __x86_64__)
+#define arch_Strcpy(dst, src, n)    strncpy(dst, src, n)
+#define arch_Memcpy(dst, src, n)	memcpy(dst, src, n)
+#elif defined(_WIN64)
+#define arch_Strcpy(dst, src, n)    strcpy_s(dst, n, src)
+#define arch_Memcpy(dst, src, n)	memcpy_s(dst, n, src, n)
+#endif
 
 
 ///Get Home Path
 ///parameter:	homePath - char* to save home path
 ///return:		true - success; false - error
-bool arch_GetHomePath(char*);
+bool arch_GetHomePath(char* homePath);
 ///Sleep for Milliseconds
-///parameter:	milliseconds
-void arch_Sleep(uint64_t);
-
+///parameter:	msec - milliseconds
+void arch_Sleep(uint64_t msec);
+///Get Time Stamp
+///return:		timestamp
 uint64_t arch_GetTimeStamp();
+///String to Timestamp
+///parameter:	timestr - datetime string as: yyyy-MM-dd hh:mm:ss.zzz
+///return:		timestamp
+uint64_t arch_Str2TimeStamp(char* timestr);
+///String to Timestamp
+///parameter:	date - yyyyMMdd
+///				time - hh:mm:ss
+///				ms - zzz
+///return:		timestamp
+uint64_t arch_Str2TimeStamp(char* date, char* time, int ms);
+///Timestamp to String
+///parameter:	buff - buffer to save string
+///				ts - timestamp
+///return:		true - success; false - error
+bool arch_TimeStamp2Str(char* buff, uint64_t ts);
 
 
 
