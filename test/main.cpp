54abd76fe63ee6e49f9a28272ce60913fb9d6044
Xxiong <xxiong@cqu.edu.cn>
2018-12-22 17:25:28 +0800

    unit test

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,6 @@
-#include "const.hpp"
-#include "log.hpp"
-#include "arch.hpp"
+#include "test_log.h"
+#include "test_arch.h"
+#include "test_config.h"
 #include <gtest/gtest.h>
 
 
@@ -8,6 +8,16 @@ TEST(test_arch, test_arch_case_1) {
 	ASSERT_TRUE(test_arch_GetHomePath());
 	ASSERT_TRUE(test_arch_Sleep());
 	ASSERT_TRUE(test_arch_GetTimeStamp());
+	ASSERT_TRUE(test_arch_Str2TimeStamp());
+	ASSERT_TRUE(test_arch_TimeStamp2Str());
+}
+
+TEST(test_log, test_log_case_1) {
+	ASSERT_TRUE(test_Log());
+}
+
+TEST(test_config, test_config_case_1) {
+	ASSERT_TRUE(test_Config());
 }
 
 int main(int argc, char**argv) {
