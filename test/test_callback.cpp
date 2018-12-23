a813a3685639ca135a0999edf41efc9ca8399b55
Xxiong <xxiong@cqu.edu.cn>
2018-12-23 22:45:56 +0800

    unit test: add callback test

---
+++ test/test_callback.cpp
@@ -0,0 +1,76 @@
+#include "test_callback.h"
+#include "../optsp-ctpcore/arch.h"
+#include "../optsp-ctpcore/config.h"
+
+static int gcnt = 0;
+
+_Logger2::_Logger2(Logger * logger, int cnt)
+	: logger(logger), cnt(cnt)
+{
+}
+
+
+void _Logger2::run() {
+	srand((int)time(0));
+	for (int i = 0; i < cnt; i++) {
+		std::stringstream log;
+		log << i;
+		logger->logging(log, __FUNCTION__, rand() % 5 + 1);
+	}
+}
+
+
+TestClass::TestClass(int id) 
+	: id(id)
+{
+	pfn = this;
+	queue = new boost::lockfree::queue<BuffField>(256);
+}
+
+
+void TestClass::run() {
+	while (true) {
+		while (queue->empty())
+			arch_Sleep(2);
+		BuffField buf;
+		queue->pop(buf);
+		gcnt++;
+	}
+}
+
+
+void TestClass::callback(int level, const char *ptr) {
+	BuffField buf;
+	arch_Strcpy(buf.data, ptr, 1024);
+	queue->push(buf);
+}
+
+
+void callback(int level, const char* buff) {
+	TestClass *x = (TestClass*)pfn;
+	x->callback(level, buff);
+}
+
+
+int test_Callback(int cnt) {
+	TestClass *tc = new TestClass(1);
+	boost::thread thrd0(boost::bind(&TestClass::run, tc));
+
+	Config *config = new Config();
+	std::string logpath = (config->homepath / "running-debug.log").string();
+
+	Logger * logger = new Logger(logpath, LOG_LEVEL_TRACE, callback);
+	boost::thread thrd1(boost::bind(&Logger::run, logger));
+
+	_Logger2 * _logger = new _Logger2(logger, cnt);
+	boost::thread thrd2(boost::bind(&_Logger2::run, _logger));
+
+	arch_Sleep(cnt);
+	thrd0.interrupt();
+	thrd1.interrupt();
+	thrd2.interrupt();
+	thrd0.join();
+	thrd1.join();
+	thrd2.join();
+	return gcnt;
+}
---
+++ test/test_callback.h
@@ -0,0 +1,40 @@
+#ifndef OPTSP_CTPCORE_TEST_CALLBACK_H_
+#define OPTSP_CTPCORE_TEST_CALLBACK_H_
+
+#include <boost/thread/thread.hpp>
+#include <boost/lockfree/queue.hpp>
+#include "../optsp-ctpcore/logger.h"
+
+static void* pfn = nullptr;
+
+typedef struct BuffField {
+	char data[1024];
+} BuffField;
+
+class TestClass 
+{
+private:
+	int id;
+	boost::lockfree::queue<BuffField> *queue = nullptr;
+
+public:
+	TestClass(int id);
+	void run();
+	void callback(int level, const char *ptr);
+};
+
+
+class _Logger2
+{
+private:
+	Logger * logger = nullptr;
+	int cnt;
+public:
+	_Logger2(Logger * logger, int cnt);
+	void run();
+};
+
+int test_Callback(int cnt);
+
+#endif // !OPTSP_CTPCORE_TEST_CALLBACK_H_
+
