19e4e0d2850362056f48ffbfd265466b5671964a
Xxiong <xxiong@cqu.edu.cn>
2018-12-23 21:27:50 +0800

    logger: accept logger module

--- optsp-ctpcore/logger.cpp
+++ optsp-ctpcore/logger.cpp
@@ -1,15 +1,17 @@
 #include "logger.h"
 #include <boost/function.hpp>
+#include <fstream>
 #include "arch.h"
 
-Logger::Logger(std::string logpath, int loglevel)
-	: logpath(logpath), loglevel(loglevel)
+
+Logger::Logger(std::string logpath, int loglevel, log_callback_fn fn)
+	: logpath(logpath), loglevel(loglevel), log_callback(fn)
 {
-	logQueue = new boost::lockfree::queue<LogCellField>(64);
+	logQueue = new boost::lockfree::queue<LogCellField>(256);
 }
 
 
-void Logger::logging(std::string &log, const char* caller, int level)
+void Logger::_logging(std::string &log, const char* caller, int level)
 {
 	if (this->loglevel < level)
 		return;
@@ -25,20 +27,55 @@ void Logger::logging(std::string &log, const char* caller, int level)
 void Logger::logging(std::stringstream &log, const char* caller, int level)
 {
 	std::string _log = log.str();
-	logging(_log, caller, level);
+	_logging(_log, caller, level);
 	log.str("");
 }
 
 
 void Logger::run()
 {
-	while (true) {
-		LogCellField logcell;
-		while (logQueue->empty())
-			arch_Sleep(2);
-		logQueue->pop(logcell);
+	DateTimeType time;
+	std::stringstream log;
+	LogCellField logcell;
+	std::ofstream out;
+	out.open(logpath, std::ios::app);
+	uint64_t currpos = arch_GetTimeStamp();
+	uint64_t lastpos = currpos;
 
+	while (true) {
+		while (logQueue->empty()) {
+			currpos = arch_GetTimeStamp();
+			if (currpos - lastpos > LOGGER_FLUSH_INTERVAL) {
+				out.close();
+				out.open(logpath, std::ios::app);
+				lastpos = currpos;
+			}
+			arch_Sleep(2);
+		}
+		logQueue->pop(logcell);
+		
+		log.str("");
+		arch_TimeStamp2Str(time, logcell.timestamp);
+		log << time;
+		switch (logcell.level) {
+		case LOG_LEVEL_ERROR:
+			log << " [ERROR] <"; break;
+		case LOG_LEVEL_WARN:
+			log << " [WARN]  <"; break;
+		case LOG_LEVEL_INFO:
+			log << " [INFO]  <"; break;
+		case LOG_LEVEL_DEBUG:
+			log << " [DEBUG] <"; break;
+		case LOG_LEVEL_TRACE:
+			log << " [TRACE] <"; break;
+		default:
+			continue;
+		}
+		log << logcell.caller << "> " << logcell.log << std::endl;
+		out << log.str().c_str();
+		(*log_callback) (logcell.level, log.str().c_str());
 	}
+	out.close();
 }
 
 
