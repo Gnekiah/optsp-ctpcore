0465437f40a7ffee1b68badc86cb6b35d661002f
Xxiong <xxiong@cqu.edu.cn>
2018-12-27 23:04:08 +0800

    platctp: rewrite message queue, reorder quoter and trader

--- optsp-ctpcore/platctp.cpp
+++ optsp-ctpcore/platctp.cpp
@@ -3,11 +3,15 @@
 #include "arch.h"
 
 
-PlatCtp::PlatCtp(Config * config, Logger * logger, quote_callback_fn qfn, trade_callback_fn tfn, cmd_callback_fn pfn)
-	: config(config), logger(logger), quote_callback(qfn), trade_callback(tfn), cmd_callback(pfn)
+PlatCtp::PlatCtp(Config * config, Logger * logger, quote_callback_fn qfn, trade_callback_fn tfn, plat_callback_fn pfn)
+	: config(config), logger(logger), quote_callback(qfn), trade_callback(tfn), plat_callback(pfn)
 {
 	std::stringstream log;
 	cmdfn = this;
+
+	quoteState = QUOTE_STATE_NONE;
+	tradeState = TRADE_STATE_NONE;
+	nInstrumentID = 0;
 	cmdQueue = new boost::lockfree::queue<PlatCmdField>(64);
 
 	quoteSpi = new QuoteSpi(logger, quote_callback, CmdCallBack);
@@ -28,41 +32,48 @@ PlatCtp::PlatCtp(Config * config, Logger * logger, quote_callback_fn qfn, trade_
 }
 
 
-void PlatCtp::InsertCommand(int rqtype, int rqid, void* ptr)
-{
-	PlatCmdField cmd = { 0 };
-	cmd.Type = rqtype;
-	cmd.Id = rqid;
-	cmd.Flag = true;
-	switch (rqtype) {
-	case CMD_TRADE_QRY_INSTRUMENT:
-		arch_Memcpy(&cmd.Instrument, ptr, sizeof(cmd.Instrument));
-		break;
-	}
-
-	lock.lock();
-	cmdQueue->push(cmd);
-	lock.unlock();
-}
-
-
 void PlatCtp::InsertCommand(int cmdtype, int cmdid, bool flag, void* ptr)
 {
 	PlatCmdField cmd = { 0 };
 	cmd.Type = cmdtype;
 	cmd.Id = cmdid;
 	cmd.Flag = flag;
-	if (flag) {
-		///Do copy from ptr to cmd.data
-		///ptr is always empty in current version
-		switch (cmdtype) {
+	switch (cmdtype) {
+	/* QuoteSpi Callback */
+	case CB_CMD_QUOTE_LOGIN:						///QuoteSpi发来的请求登录通知
+		quoteState = QUOTE_STATE_CONNECTED;
+		break;
+	case CB_CMD_QUOTE_SUBSCRIBE:					///QuoteSpi发来的请求订阅通知，订阅操作要等tradeReady之后才进行
+		quoteState = QUOTE_STATE_LOGINED;
+		break;
 
-		}
+	/* TradeSpi Callback */
+	case CB_CMD_TRADE_AUTHENTICATE:					///TradeSpi发来的请求验证通知
+		tradeState = TRADE_STATE_CONNECTED;
+		break;
+	case CB_CMD_TRADE_LOGIN:						///TradeSpi发来的请求登录通知
+		tradeState = TRADE_STATE_AUTHENED;
+		break;
+	case CB_CMD_TRADE_SETTLEMENT_CONFIRM:			///TradeSpi发来的请求结算确认通知
+		tradeState = TRADE_STATE_LOGINED;
+		break;
+	case CB_CMD_TRADE_QRY_INSTRUMENT:				///TradeSpi发来的请求查询合约通知
+		tradeState = TRADE_STATE_SETTLEMENT_CONFIRMED;
+		break;
+	case CB_CMD_TRADE_RSP_QRY_INSTRUMENT:			///TradeSpi发来的合约编号数据，只用于控制quoteApi执行订阅
+		arch_Memcpy(instrumentIDs[nInstrumentID], ptr, sizeof(InstrumentField));
+		nInstrumentID++;
+		return;
+	case CB_CMD_TRADE_QRY_INSTRUMENT_COMPLETED:		///TradeSpi发来的合约列表获取完毕的通知，获取该通知后，直接修改tradeReady，不插入命令队列
+		tradeState = TRADE_STATE_INSTRUMENT_QUERIED;
+		return;
+
+	/* STGroup Command */
+
+	default:
+		return;
 	}
-
-	lock.lock();
 	cmdQueue->push(cmd);
-	lock.unlock();
 }
 
 
@@ -70,20 +81,59 @@ void PlatCtp::run()
 {
 	int ret = 0;
 	std::stringstream log;
+	PlatCmdField cmd = { 0 };
+
+	while (!config->ready)
+		arch_Sleep(100);
+
 	quoteApi->Init();
 	tradeApi->Init();
+
+	log << config->brokerID << ";" << config->userID << ";" << config->investorID << ";" << config->password << ";" << config->quoteFrontAddr << ";" << config->tradeFrontAddr;
+	LOGINFO(logger, log);
 
 	log << "Looping...";
 	LOGINFO(logger, log);
 
 	while (true) {
+		while (cmdQueue->empty())
+			arch_Sleep(10);
+		cmdQueue->pop(cmd);
+
+		switch (cmd.Type) {
+		/* QuoteSpi Command */
+		case CB_CMD_QUOTE_LOGIN:
+			ret = DoQuoteLogin(cmd);
+			break;
+		case CB_CMD_QUOTE_SUBSCRIBE:
+			ret = DoQuoteSubscribe(cmd);
+			break;
+		/* TradeSpi Command */
+		case CB_CMD_TRADE_AUTHENTICATE:
+			ret = DoTradeAuthenticate(cmd);
+			break;
+		case CB_CMD_TRADE_LOGIN:
+			ret = DoTradeLogin(cmd);
+			break;
+		case CB_CMD_TRADE_SETTLEMENT_CONFIRM:
+			ret = DoTradeSettlementConfirm(cmd);
+			break;
+		case CB_CMD_TRADE_QRY_INSTRUMENT:
+			ret = DoTradeQryInstrument(cmd);
+			break;
+		/* StGroup Command */
+
+		default:
+			ret = 0;
+			break;
+		}
+		
+		if (cmd.Id != CMDID_QUOTE && cmd.Id != CMDID_TRADE) {
+			(*plat_callback)(cmd.Type, cmd.Id, (ret == 0 ? true : false), nullptr);
+		}
 		arch_Sleep(500);
-
-
 	}
-
-	///TODO: looping
-
+	
 	quoteApi->Join();
 	tradeApi->Join();
 	delete quoteSpi;
@@ -93,6 +143,133 @@ void PlatCtp::run()
 }
 
 
+
+int PlatCtp::DoQuoteLogin(PlatCmdField &cmd)
+{
+	int ret = 0;
+	std::stringstream log;
+
+	CThostFtdcReqUserLoginField req = { 0 };
+	arch_Strcpy(req.UserID, config->userID, sizeof(req.UserID));
+	arch_Strcpy(req.BrokerID, config->brokerID, sizeof(req.BrokerID));
+	arch_Strcpy(req.Password, config->password, sizeof(req.Password));
+	ret = quoteApi->ReqUserLogin(&req, cmd.Id);
+	if (ret) {
+		log << "Failed to login quote front server, nRequest=" << cmd.Id;
+		LOGERR(logger, log);
+	}
+	return ret;
+}
+
+
+int PlatCtp::DoQuoteSubscribe(PlatCmdField &cmd)
+{
+	int ret = 0;
+	std::stringstream log;
+
+	if (tradeState < TRADE_STATE_INSTRUMENT_QUERIED) {
+		InsertCommand(CB_CMD_QUOTE_SUBSCRIBE, CMDID_QUOTE, false, nullptr);
+		return -1;
+	}
+	ret = quoteApi->SubscribeMarketData(instrumentIDs, nInstrumentID);
+	if (ret) {
+		log << "Failed to subscribe market data, nRequest=" << cmd.Id;
+		LOGERR(logger, log);
+	}
+	return ret;
+}
+
+
+int PlatCtp::DoTradeAuthenticate(PlatCmdField &cmd)
+{
+	int ret = 0;
+	std::stringstream log;
+	
+	CThostFtdcReqAuthenticateField req = { 0 };
+	arch_Strcpy(req.AuthCode, config->AUTH_CODE, sizeof(req.AuthCode));
+	arch_Strcpy(req.UserProductInfo, config->APP_NAME, sizeof(req.UserProductInfo));
+	arch_Strcpy(req.BrokerID, config->brokerID, sizeof(req.BrokerID));
+	arch_Strcpy(req.UserID, config->userID, sizeof(req.UserID));
+	ret = tradeApi->ReqAuthenticate(&req, cmd.Id);
+	if (ret) {
+		log << "Failed to do trade authenticate, nRequestID=" << cmd.Id;
+		LOGERR(logger, log);
+	}
+	return ret;
+}
+
+
+int PlatCtp::DoTradeLogin(PlatCmdField &cmd)
+{
+	int ret = 0;
+	std::stringstream log;
+
+	CThostFtdcReqUserLoginField req = { 0 };
+	arch_Strcpy(req.UserID, config->userID, sizeof(req.UserID));
+	arch_Strcpy(req.BrokerID, config->brokerID, sizeof(req.BrokerID));
+	arch_Strcpy(req.Password, config->password, sizeof(req.Password));
+	ret = tradeApi->ReqUserLogin(&req, cmd.Id);
+	if (ret) {
+		log << "Failed to login trade front server, nRequestID=" << cmd.Id;
+		LOGERR(logger, log);
+	}
+	return ret;
+}
+
+
+int PlatCtp::DoTradeSettlementConfirm(PlatCmdField &cmd)
+{
+	int ret = 0;
+	std::stringstream log;
+
+	CThostFtdcQrySettlementInfoConfirmField req1 = { 0 };
+	arch_Strcpy(req1.BrokerID, config->brokerID, sizeof(req1.BrokerID));
+	arch_Strcpy(req1.InvestorID, config->investorID, sizeof(req1.InvestorID));
+	ret = tradeApi->ReqQrySettlementInfoConfirm(&req1, cmd.Id);
+	if (ret) {
+		log << "Failed to query settlement info confirm";
+		LOGERR(logger, log);
+	}
+	arch_Sleep(500);
+	CThostFtdcQrySettlementInfoField req2 = { 0 };
+	arch_Strcpy(req2.BrokerID, config->brokerID, sizeof(req2.BrokerID));
+	arch_Strcpy(req2.InvestorID, config->investorID, sizeof(req2.InvestorID));
+	ret = tradeApi->ReqQrySettlementInfo(&req2, cmd.Id);
+	if (ret) {
+		log << "Failed to query settlement info";
+		LOGERR(logger, log);
+	}
+	arch_Sleep(500);
+	CThostFtdcSettlementInfoConfirmField req3 = { 0 };
+	arch_Strcpy(req3.BrokerID, config->brokerID, sizeof(req3.BrokerID));
+	arch_Strcpy(req3.InvestorID, config->investorID, sizeof(req3.InvestorID));
+	ret = tradeApi->ReqSettlementInfoConfirm(&req3, cmd.Id);
+	if (ret) {
+		log << "Failed to do settlement info confirm";
+		LOGERR(logger, log);
+	}
+	return ret;
+}
+
+
+int PlatCtp::DoTradeQryInstrument(PlatCmdField &cmd)
+{
+	int ret = 0;
+	std::stringstream log;
+
+	CThostFtdcQryInstrumentField req = { 0 };
+	ret = tradeApi->ReqQryInstrument(&req, cmd.Id);
+	if (ret) {
+		log << "Failed to query instrument";
+		LOGWARN(logger, log);
+	}
+	return ret;
+}
+
+
+
+
+///for callback
 void CmdCallBack(int cmdtype, int cmdid, bool flag, void* ptr)
 {
 	PlatCtp *plat = (PlatCtp*)cmdfn;
--- optsp-ctpcore/platctp.h
+++ optsp-ctpcore/platctp.h
@@ -5,10 +5,11 @@
 #include "ctpapi/ThostFtdcTraderApi.h"
 #include "ctpapi/ThostFtdcUserApiDataType.h"
 #include "ctpapi/ThostFtdcUserApiStruct.h"
-#pragma comment (lib, "ctpapi/thostmduserapi.lib")
-#pragma comment (lib, "ctpapi/thosttraderapi.lib")
+//#pragma comment (lib, "ctpapi/thostmduserapi.lib")
+//#pragma comment (lib, "ctpapi/thosttraderapi.lib")
 
 #include <mutex>
+#include <atomic>
 #include <boost/lockfree/queue.hpp>
 #include "quotespi.h"
 #include "tradespi.h"
@@ -26,36 +27,45 @@ class PlatCtp
 private:
 	Config * config = nullptr;
 	Logger * logger = nullptr;
-
+	///called by spi
 	quote_callback_fn quote_callback = nullptr;
 	trade_callback_fn trade_callback = nullptr;
-	cmd_callback_fn cmd_callback = nullptr;
+	///called by current instance
+	plat_callback_fn plat_callback = nullptr;
 
 	QuoteSpi * quoteSpi = nullptr;
 	TradeSpi * tradeSpi = nullptr;
 	CThostFtdcMdApi * quoteApi = nullptr;
 	CThostFtdcTraderApi * tradeApi = nullptr;
 
-	std::mutex lock;
+	int nInstrumentID;
+	char *instrumentIDs[PLATCTP_INSTRUMENT_MAX];
 	boost::lockfree::queue<PlatCmdField> *cmdQueue = nullptr;
 
 public:
-	PlatCtp(Config * config, Logger * logger, quote_callback_fn qfn, trade_callback_fn tfn, cmd_callback_fn pfn);
-	void InsertCommand(int rqtype, int rqid, void* ptr);					///Insert cmd. by upper ST. Group
-	void InsertCommand(int cmdtype, int cmdid, bool flag, void* ptr);		///Insert cmd. from lower plat.
+	std::atomic_int quoteState;		///标记QuoteApi的状态
+	std::atomic_int tradeState;		///标记TradeApi的状态
+
+public:
+	PlatCtp(Config * config, Logger * logger, quote_callback_fn qfn, trade_callback_fn tfn, plat_callback_fn pfn);
+	///Insert cmd. into plat.
+	///int cmdtype - CMD的类型
+	///int cmdid - 命令的ID, 由下发命令时分配
+	///bool flag - 标记数据是否有效
+	///void* ptr - 指向数据区域的首地址指针
+	void InsertCommand(int cmdtype, int cmdid, bool flag, void* ptr);
 	void run();
 
 private:
 	///Quote cmd.
-	int DoQuoteLogin();
-	int DoQuoteSubscribe();
-	int DoQuoteSubscribeAll();
+	int DoQuoteLogin(PlatCmdField &);
+	int DoQuoteSubscribe(PlatCmdField &);
 
 	///Trade cmd.
-	int DoTradeAuthenticate();
-	int DoTradeLogin();
-	int DoTradeSettlementInfoConfirm();
-	int DoTradeQryInstrument();
+	int DoTradeAuthenticate(PlatCmdField &);
+	int DoTradeLogin(PlatCmdField &);
+	int DoTradeSettlementConfirm(PlatCmdField &);
+	int DoTradeQryInstrument(PlatCmdField &);
 };
 
 #endif // !OPTSP_CTPCORE_PLATCTP_H_
